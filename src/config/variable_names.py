085f831f2ba64e46ee57c91888816d67bf34692e
Anton Zimin <zimkaa87@gmail.com>
2023-04-22 16:21:53 +0300

    refactor
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- src/config/variable_names.py
+++ src/config/variable_names.py
@@ -4,8 +4,3 @@ LEADER = "leader"
 
 MAG = "mag"
 WARRIOR = "warrior"
-
-HUNTER = "HunT3R"
-ROMANSON = "D@rk_Side"
-TURNOFF = "TuRnOff"
-ANGEL = "Ангел"
---
+++ src/elixir/elixir.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Elixir(str, Enum):
+    ELIXIR_OF_RESTORATION = "101"  # Элексир восстановления
+    BOT_BAIT = "102"  # Приманку Для Ботов
+    CHAMPAGNE_BOTTLE = "103"  # Бутылку Шампанского
+    INSTANT_HEALING_ELIXIR = "104"  # Эликсир Мгновенного Исцеления
+    POTION_BLOODTHIRSTY = "105"  # Зелье Кровожадности
+    ELIXIR_OF_SWIFTNESS = "106"  # Эликсир Быстроты
+    BLISS_ELIXIR = "107"  # Эликсир Блаженства
+    ILANAS_GIFT = "108"  # Дар Иланы
+    SNOWDROP_ELIXIR = "109"  # Эликсир из Подснежника
+    ELIXIR_OF_YOUTH = "110"  # Молодильное яблочко
+    FAROS_WINE = "156"  # Фаросское Вино
+    PLANAR_SUMMONING = "152"  # Планарный призыв!!!
+    FIRE_ENHANCEMENT = "161"  # Усиление огня
