07b20623d9a11177bf370f4a1f79bfc500d68c0c
Anton Zimin <zimkaa87@gmail.com>
2023-04-30 15:42:43 +0300

    added new envs
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- tests/os_env_fixture.py
+++ tests/os_env_fixture.py
@@ -7,3 +7,11 @@ def mock_env_user(monkeypatch):
         "CHECKER_IP_SITE",
         "http://testip.example",
     )
+    monkeypatch.setenv(
+        "TG_TOKEN",
+        "example:token",
+    )
+    monkeypatch.setenv(
+        "CHANNEL_ID",
+        "1111111",
+    )
